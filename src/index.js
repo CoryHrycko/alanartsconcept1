2551d9e3e18e85cee572af6bca04267ba085f044
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-07 00:03:00 -0500

    t

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-import React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import store from "./store/index";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './store';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
 
-render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById("app")
+ReactDOM.render(
+ <Provider store={configureStore()}>
+  <App />
+ </Provider>,
+ document.getElementById('root')
 );
+registerServiceWorker();
