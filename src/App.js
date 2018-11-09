c0bbbcdf42590ab9ee874f00f1220a9a03dbe785
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-08 22:18:13 -0500

    outlining the main divs

--- src/App.js
+++ src/App.js
@@ -20,11 +20,13 @@ class App extends Component {
  render() {
   return (
    <div className="App">
-    <header className="heropanel--video" data-vide-bg="mp4: ../assests/css/ThePiano.mp4"
-    datavi>
-     <img src={Capture} className="App-logo" alt="logo" />
-     <h1 className="App-title">Allen Arts Music</h1>
-     </header>
+    <header className="heropanel">
+      <div className="v-header container">
+        <video src="../assests/css/ThePiano.mov" autoPlay={true} loop={true}></video>
+          <img src={Capture} className="App-logo" alt="logo" />
+          <h1 className="App-title">Allen Arts Music</h1>
+      </div>
+    </header>
      <container className="mainShape">
    
      <h3 className="mainShapeTitle"> In the back is either going to be a. a series of videos or slide show</h3>
