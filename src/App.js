fb1f2d5be240df8e486548c1b452a31a07ec318b
chrycko9491@gmail.com <chrycko9491@gmail.com>
2018-11-09 22:56:08 -0500

    more wire framming added youtube support

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { simpleAction } from './actions/simpleAction'
 import Toolbar from './components/Toolbar/Toolbar.jsx';
 import SideDrawer from './components/SideDrawer/SideDrawer.jsx'
 import Backdrop from './components/Backdrop/Backdrop.jsx';
-import Video from './components/bgVideo'
+// import Video from './components/bgVideo'
 
 const mapStateToProps = state => ({
   ...state
@@ -47,7 +47,7 @@ backdropClickHandler=()=>{
     {/* <video autoPlay muted loop id="myVideo"> 
       <source src="https://youtu.be/zmPzbZVUp3g" type="video/mp4"></source>
     </video>*/}
-      <Video />
+      {/* <Video /> */}
       <div className="v-header container">
           <img src={Capture} className="App-logo" alt="logo" />
           <h1 className="App-title">Allen Arts Music</h1>
@@ -58,16 +58,27 @@ backdropClickHandler=()=>{
 
     </header>
      <div className="mainShape">
-   
-     <h3 className="mainShapeTitle"> In the back is either going to be a. a series of videos or slide show</h3>
+      <div>
+     <h3 className="mainShapeTitle"> In the back is either going to be a. a series of videos or slide show carosal</h3>
+    
 
      </div>
+     <div>
+     <h3 className="mainShapeTitle">> some sort of Events</h3>
    </div>
-   
+   <div>
+     <h3 className="mainShapeTitle">> a claender of events</h3>
+   </div>
+   <div>
+     <h3 className="mainShapeTitle">> Contact Form</h3>
+   </div>
+   </div>
+   <footer>some text displayinh date</footer>>
+   </div>
     
   );
  }
-}
+};
 
 
  
